3bda58ae889f5425dd0491679af09e9cb613d6c4
Yancey Leo <yanceyofficial@gmail.com>
2020-12-06 21:50:33 +0800

    feat: update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'blocs/bloc_observer_bus.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = BlocObserverBus();
   runApp(App());
 }

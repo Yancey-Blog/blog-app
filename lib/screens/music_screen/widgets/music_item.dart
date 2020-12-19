a5a2f916965247b73f1a7baff62c0ffe20883dfd
Yancey Leo <yanceyofficial@gmail.com>
2020-12-19 21:34:51 +0800

    feat: update

--- lib/screens/music_screen/widgets/music_item.dart
+++ lib/screens/music_screen/widgets/music_item.dart
@@ -15,68 +15,69 @@ class MusicItem extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              Container(
-                width: 24,
-                height: 24,
-                child: Center(
-                  child: Text(
-                    formatIndex(index),
-                    style: TextStyle(
-                      color: Color(0xff93a8b3),
-                      fontSize: 12,
-                      letterSpacing: .26,
+          Container(
+            width: 24,
+            height: 24,
+            child: Center(
+              child: Text(
+                formatIndex(index),
+                style: TextStyle(
+                  color: Color(0xff93a8b3),
+                  fontSize: 12,
+                  letterSpacing: .26,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 12, left: 6),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(4),
+                    child: Image.asset(
+                      'assets/images/music_screen/top-bg@3x.png',
+                      width: 40,
+                      height: 40,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 12, left: 6),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(4),
-                  child: Image.asset(
-                    'assets/images/music_screen/top-bg@3x.png',
-                    width: 40,
-                    height: 40,
-                    fit: BoxFit.cover,
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 20,
+                      child: Center(
+                        child: Text(
+                          'Mirage',
+                          style: TextStyle(
+                            color: Color(0xff3c425b),
+                            fontSize: 14,
+                            letterSpacing: .26,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 14,
+                      child: Center(
+                        child: Text(
+                          'Else',
+                          style: TextStyle(
+                            color: Color(0xff93a8b3),
+                            fontSize: 12,
+                            letterSpacing: .26,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 20,
-                    child: Center(
-                      child: Text(
-                        'Mirage',
-                        style: TextStyle(
-                          color: Color(0xff3c425b),
-                          fontSize: 14,
-                          letterSpacing: .26,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 14,
-                    child: Center(
-                      child: Text(
-                        'Else',
-                        style: TextStyle(
-                          color: Color(0xff93a8b3),
-                          fontSize: 12,
-                          letterSpacing: .26,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
           Center(
             child: Text(

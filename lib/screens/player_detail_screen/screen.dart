f26c1694ebdeb3333cdcf5daae80381249b34ac6
Yancey Leo <yanceyofficial@gmail.com>
2020-12-23 21:15:51 +0800

    feat: update

--- lib/screens/player_detail_screen/screen.dart
+++ lib/screens/player_detail_screen/screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:blog_app/repositories/repositories.dart';
+import 'package:blog_app/blocs/blocs.dart';
 import 'package:blog_app/models/models.dart';
+
 import 'view.dart';
 
 class PlayerDetailScreen extends StatelessWidget {
+  final playerRepository = PlayerRepository();
+
   PlayerDetailScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final player = ModalRoute.of(context).settings.arguments as Player;
-    return PlayerDetailView(player: player);
+
+    return BlocProvider(
+      create: (context) => PlayerControllerBloc(),
+      child: BlocBuilder<PlayerControllerBloc, PlayerControllerState>(
+        builder: (context, state) {
+          return PlayerDetailView(player: player);
+        },
+      ),
+    );
   }
 }
--- lib/screens/player_detail_screen/widgets/play_control.dart
+++ lib/screens/player_detail_screen/widgets/play_control.dart
@@ -1,54 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-class PlayControl extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+import 'package:blog_app/models/models.dart';
+import 'package:blog_app/blocs/blocs.dart';
+
+enum PlayerStatus { Playing, Paused, Stopped }
+
+class PlayControl extends StatefulWidget {
+  final Player player;
+
+  PlayControl({
+    Key key,
+    @required this.player,
+  }) : super(key: key);
+
+  @override
+  _PlayControlState createState() => _PlayControlState();
+}
+
+class _PlayControlState extends State<PlayControl> {
+  PlayerStatus _playerStatus = PlayerStatus.Stopped;
+
+  AudioPlayer audioPlayer = AudioPlayer(playerId: '');
+
+  void play(Player player) async {
+    await audioPlayer.play(player.musicFileUrl);
+
+    setState(() {
+      _playerStatus = PlayerStatus.Playing;
+    });
+  }
+
+  void pause() async {
+    await audioPlayer.pause();
+
+    setState(() {
+      _playerStatus = PlayerStatus.Paused;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Icon(
-          CupertinoIcons.arrow_2_circlepath,
-        ),
-        Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                shape: BoxShape.circle,
+    return BlocBuilder<PlayerControllerBloc, PlayerControllerState>(
+        builder: (context, state) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            CupertinoIcons.arrow_2_circlepath,
+          ),
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  CupertinoIcons.backward_end,
+                ),
               ),
-              child: Icon(
-                CupertinoIcons.backward_end,
+              Container(
+                margin: const EdgeInsets.only(left: 24, right: 24),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  shape: BoxShape.circle,
+                ),
+                child: _playerStatus == PlayerStatus.Paused
+                    ? IconButton(
+                        icon: Icon(Icons.pause),
+                        onPressed: () => play(widget.player),
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.play_arrow),
+                        onPressed: pause,
+                      ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 24, right: 24),
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                shape: BoxShape.circle,
+              Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  CupertinoIcons.arrow_swap,
+                ),
               ),
-              child: Icon(
-                Icons.play_arrow,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                CupertinoIcons.arrow_swap,
-              ),
-            ),
-          ],
-        ),
-        Icon(
-          CupertinoIcons.backward_end,
-        ),
-      ],
-    );
+            ],
+          ),
+          Icon(
+            CupertinoIcons.backward_end,
+          ),
+        ],
+      );
+    });
   }
 }

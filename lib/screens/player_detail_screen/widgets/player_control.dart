5250b86bb630bbb6a2f3eed7d27ffdedd3fdcb25
Yancey Leo <yanceyofficial@gmail.com>
2020-12-24 00:02:35 +0800

    feat: update PlayerControl

--- lib/screens/player_detail_screen/widgets/player_control.dart
+++ lib/screens/player_detail_screen/widgets/player_control.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'package:blog_app/models/models.dart';
+
 import './play_control.dart';
 import './volume_control.dart';
 
 class PlayerControl extends StatelessWidget {
+  final Player player;
+
+  const PlayerControl({Key key, this.player}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        PlayControl(),
+        PlayControl(player: player),
         VolumeControl(),
       ],
     );

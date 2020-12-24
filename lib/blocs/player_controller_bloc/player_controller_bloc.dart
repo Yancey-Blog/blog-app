e55732e56818cebb8e772267964db2de78ae8a41
Yancey Leo <yanceyofficial@gmail.com>
2020-12-24 17:23:19 +0800

    feat: delete repository

--- lib/blocs/player_controller_bloc/player_controller_bloc.dart
+++ lib/blocs/player_controller_bloc/player_controller_bloc.dart
@@ -5,18 +5,13 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 import 'package:blog_app/models/models.dart';
-import 'package:blog_app/repositories/repositories.dart';
 
 part 'player_controller_event.dart';
 part 'player_controller_state.dart';
 
 class PlayerControllerBloc
     extends Bloc<PlayerControllerEvent, PlayerControllerState> {
-  final PlayerControllerRepository playerControllerRepository;
-
-  PlayerControllerBloc({
-    this.playerControllerRepository,
-  }) : super(PlayerControllerInitial());
+  PlayerControllerBloc() : super(PlayerControllerInitial());
 
   @override
   Stream<PlayerControllerState> mapEventToState(
@@ -24,13 +19,7 @@ class PlayerControllerBloc
   ) async* {
     if (event is AudioPlayed) {
       yield AudioLoadInProgress();
-
-      try {
-        await playerControllerRepository.play(event.audio.musicFileUrl);
-        yield AudioPlaySuccess();
-      } catch (_, stackTrace) {
-        yield AudioLoadFailure('$_ $stackTrace');
-      }
+      yield AudioPlaySuccess();
 
       if (event is AudioPaused) {
         yield AudioPauseSuccess();

e0fde40478daecf34937ff4407cb1c04a50ff1cb
Yancey Leo <yanceyofficial@gmail.com>
2020-12-25 23:30:12 +0800

    feat: gti push

--- lib/screens/player_detail_screen/widgets/control_buttons.dart
+++ lib/screens/player_detail_screen/widgets/control_buttons.dart
@@ -20,43 +20,54 @@ class ControlButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          padding: const EdgeInsets.all(4),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            shape: BoxShape.circle,
-          ),
-          child: Icon(
-            CupertinoIcons.backward_end,
-            color: Colors.black,
-          ),
+        Icon(
+          CupertinoIcons.arrow_2_circlepath,
         ),
-        Container(
-          margin: const EdgeInsets.only(left: 24, right: 24),
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            shape: BoxShape.circle,
-          ),
-          child: IconButton(
-            icon: Icon(
-              playStatus == PlayStatus.Play ? Icons.pause : Icons.play_arrow,
-              color: Colors.black,
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                CupertinoIcons.backward_end,
+                color: Colors.black,
+              ),
             ),
-            onPressed: playStatus == PlayStatus.Play ? () => pause : () => play,
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.all(4),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            shape: BoxShape.circle,
-          ),
-          child: Icon(
-            CupertinoIcons.forward_end,
-            color: Colors.black,
-          ),
+            Container(
+              margin: const EdgeInsets.only(left: 24, right: 24),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: Icon(
+                  playStatus == PlayStatus.Play
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                  color: Colors.black,
+                ),
+                onPressed:
+                    playStatus == PlayStatus.Play ? () => pause : () => play,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                CupertinoIcons.forward_end,
+                color: Colors.black,
+              ),
+            ),
+          ],
         ),
         Icon(
           CupertinoIcons.arrow_swap,
--- lib/screens/player_detail_screen/widgets/player_controllor.dart
+++ lib/screens/player_detail_screen/widgets/player_controllor.dart
@@ -92,61 +92,10 @@ class _PlayerControllorState extends State<PlayerControllor> {
             );
           },
         ),
-        ControlButtons(playStatus: _playStatus),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-              CupertinoIcons.arrow_2_circlepath,
-            ),
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    CupertinoIcons.backward_end,
-                    color: Colors.black,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 24, right: 24),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    icon: Icon(
-                      _playStatus == PlayStatus.Play
-                          ? Icons.pause
-                          : Icons.play_arrow,
-                      color: Colors.black,
-                    ),
-                    onPressed: _playStatus == PlayStatus.Play ? pause : play,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(4),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    CupertinoIcons.forward_end,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-            Icon(
-              CupertinoIcons.arrow_swap,
-              color: Colors.black,
-            ),
-          ],
+        ControlButtons(
+          playStatus: _playStatus,
+          play: play,
+          pause: pause,
         ),
         VolumeControllor(),
       ],
